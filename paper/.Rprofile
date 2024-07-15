f3d2387a294a7f6d128bf6134f79d794486653ce
Dan Hicks <hicks.daniel.j@gmail.com>
2024-07-15 10:52:40 -0700

    get terminal Quarto to recognize renv

---
+++ paper/.Rprofile
@@ -0,0 +1,5 @@
+## Jump up to the project directory to activate renv
+actual_wd = getwd()
+setwd('..')
+source('.Rprofile')
+setwd(actual_wd)
---
+++ scripts/03/06_prompts_gt.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(googlesheets4)
+library(gt)
+library(here)
+
+prompts_gt = read_sheet('https://docs.google.com/spreadsheets/d/1EuQbThPe7XCwU0SkZH9tZFy3_vTdN0vNHORpSTIbwrA/edit?gid=1444641995#gid=1444641995', 
+           sheet = 'comparison') |> 
+    mutate(modified = 
+               if_else(`original text` != `finalized text` &
+                           !is.na(`finalized text`), 
+                       '✓', 
+                       '')) |> 
+    gt() |> 
+    sub_missing(missing_text = '') |> 
+    cols_align(align = 'center', 
+               columns = modified)
+
+write_rds(prompts_gt, here('out', '03', '06_prompts.Rds'))
+gtsave(prompts_gt, here('out', '03', '06_prompts.html'))
+gtsave(prompts_gt, here('out', '03', '06_prompts.pdf'))
